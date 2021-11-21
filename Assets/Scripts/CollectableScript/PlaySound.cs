ac97b3313f01cf31807dac968aa6a3a8e94e9b19
Victor <vsenneville@yahoo.fr>
2021-11-21 09:55:39 +0100

    Import des sound effect des notes

---
+++ Assets/Scripts/CollectableScript/PlaySound.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> _guitarRiff = new List<AudioClip>();
+
+
+}
